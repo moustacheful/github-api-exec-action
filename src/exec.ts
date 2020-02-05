ef0d27179e2eed8f82b3224573dbd24d9da7922c
Daniel Acuna <suchious@gmail.com>
2020-02-05 02:08:10 -0300

    style: appease the linting gods

--- src/exec.ts
+++ src/exec.ts
@@ -1,27 +1,32 @@
 import * as core from '@actions/core'
-import { GitHub } from '@actions/github'
-import _ from 'lodash';
+import {GitHub} from '@actions/github'
+import _ from 'lodash'
 
-export async function exec(octokit: GitHub, command: string, payload: {}, resultPath?: string): Promise<{}> {
-    const fn = _.get(octokit, command, () => {
-        throw new Error(`Command '${command}' does not exist in octokit!`);
-    })
+export async function exec(
+  octokit: GitHub,
+  command: string,
+  payload: {},
+  resultPath?: string
+): Promise<{}> {
+  const fn = _.get(octokit, command, () => {
+    throw new Error(`Command '${command}' does not exist in octokit!`)
+  })
 
-    if (typeof fn !== 'function') {
-        throw new Error(`Command '${command}' is not a command to execute.`)
-    }
+  if (typeof fn !== 'function') {
+    throw new Error(`Command '${command}' is not a command to execute.`)
+  }
 
-    const result = await fn(payload).then((res: any) => res.data);
+  const result = await fn(payload).then(
+    async (res: {data: unknown}) => res.data
+  )
 
-    core.debug("resultPath: " + resultPath)
+  core.debug(`resultPath: ${resultPath}`)
 
-    if (resultPath) {
-        core.debug(
-           "extracted: " + _.get(result, resultPath)
-        )
-        
-        return _.get(result, resultPath)
-    }
+  if (resultPath) {
+    core.debug(`extracted: ${_.get(result, resultPath)}`)
 
-    return result
+    return _.get(result, resultPath)
+  }
+
+  return result
 }
